0081c6cac769d8e2c5fae5058194ce2b6c547e94
11Maks22 <m.slobodianuk@gmail.com>
2025-02-18 20:51:20 +0200

    Changed functions.ts and arrow_functions.ts files in Homework for Lesson_7

--- HW7_ts_basics/src/functions.ts
+++ HW7_ts_basics/src/functions.ts
@@ -1,17 +1,21 @@
-function sumElements(numbers: number[]): number {
-    return numbers.reduce((sum, num) => sum + num, 0);
-}
-
+// function sumElements(numbers: number[]): number {
+//     return numbers.reduce((sum, num) => sum + num, 0);
+// }
+export const sumElements = (numbers: number[]): number => numbers.reduce((sum, num) => sum + num, 0);
 const myArr: number[] = [1, 5, 8, 8];
 const result: number = sumElements(myArr);
 
 console.log(result);
 
-function sumArray(numbers: (number | string)[]): number {
-    return numbers
-        .filter((num): num is number => typeof num === "number")
+export const sumArray = (numbers: (number | string)[]): number =>
+    numbers
+        .filter((num): num is number => typeof num === 'number')
         .reduce((sum, num) => sum + num, 0);
-}
+// function sumArray(numbers: (number | string)[]): number {
+//     return numbers
+//         .filter((num): num is number => typeof num === 'number')
+//         .reduce((sum, num) => sum + num, 0);
+// }
 
 const stringArr: string[] = ['Gimli', 'Legolas', 'Frodo'];
 const numberArr: number[] = [2, 4, 6, 8];
@@ -21,3 +25,4 @@ console.log(resultNumbers);
 
 const resultStrings: number = sumArray(stringArr);
 console.log(resultStrings);
+
