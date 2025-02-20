92fba302b91021d6f27c1d1a166eac859c07e8d1
11Maks22 <m.slobodianuk@gmail.com>
2025-02-20 15:20:57 +0200

    Revert "Lesson 8"

--- HW4/src/loop.js
+++ HW4/src/loop.js
@@ -1,11 +1,11 @@
 // Cycle from 0 to 9
 console.log("Cycle from 0 to 9:");
 for (let i = 0; i < 10; i++) {
-    console.log(i);
+  console.log(i);
 }
 
 // Cycle from 100 to 0 with step 10
 console.log("\n Cycle from 100 to 0 with step 10:");
 for (let i = 100; i >= 0; i -= 10) {
-    console.log(i);
+  console.log(i);
 }
