be21f4b542bf1ec56c99abccbecfd475cde17b31
11Maks22 <m.slobodianuk@gmail.com>
2025-03-07 14:34:42 +0200

    Added Updates to HW4 - new classes and methods

--- HW4/src/objects.js
+++ HW4/src/objects.js
@@ -1,18 +1,38 @@
 const complexObject = {
     name: "Complex Object",
     details: {
-    version: 1.0,
-    author: "Your name",
+        version: 1.0,
+        author: "Maks Sloboda"
     },
     data: [1, 2, 3, 4, 5],
     printData: function() {
-    console.log("\n Data:", this.data);
+        console.log("\n Data:", this.data);
     }
-    };
-    
-    // Print object values
-    console.log("\n Object:");
-    console.log("Name:", complexObject.name);
-    console.log("Version:", complexObject.details.version);
-    console.log("Author:", complexObject.details.author);
-    complexObject.printData();
+};
+
+// Виведення об'єкта
+console.log("\n Object:");
+console.log("Name:", complexObject.name);
+console.log("Version:", complexObject.details.version);
+console.log("Author:", complexObject.details.author);
+complexObject.printData();
+
+// Використання for...in для деталей
+console.log("\n for...in (details):");
+for (const key in complexObject.details) {
+    console.log(`${key}: ${complexObject.details[key]}`);
+}
+
+// Використання Object.keys()
+console.log("\n Object.keys():");
+console.log(Object.keys(complexObject));
+
+// Використання Object.values()
+console.log("\n Object.values():");
+console.log(Object.values(complexObject));
+
+// Використання Object.entries()
+console.log("\n Object.entries():");
+Object.entries(complexObject).forEach(([key, value]) => {
+    console.log(`${key}:`, value);
+});
