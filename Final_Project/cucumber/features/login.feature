9ca412eb556e4dbcd3995db3038a8e9588ff8e52
11Maks22 <m.slobodianuk@gmail.com>
2025-04-23 23:46:32 +0300

    A very few path of Final PJ

---
+++ Final_Project/cucumber/features/login.feature
@@ -0,0 +1,10 @@
+Feature: User login to FOPHelp
+
+Scenario: Користувач успішно авторизується на сайті FOPHelp
+  Given я відкриваю головну сторінку FOPHelp
+  When я клікаю на кнопку "Увійти"
+  And я вводжу email "m.slobodianuk@gmail.com"
+  And я вводжу пароль "aN8\"bX6\`cQ7;"
+  And я натискаю кнопку входу
+  Then я повинен побачити, що авторизація пройшла успішно
+
---
+++ Final_Project/cucumber/reporter.js
@@ -0,0 +1,24 @@
+const reporter = require('multiple-cucumber-html-reporter');
+
+reporter.generate({
+  jsonDir: './reports/json',
+  reportPath: './reports/html',
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: 'latest'
+    },
+    device: 'Local test machine',
+    platform: {
+      name: 'windows',
+      version: '10'
+    }
+  },
+  customData: {
+    title: 'Final Project QA Automation Report',
+    data: [
+      { label: 'Project', value: 'FOPHelp E2E Tests' },
+      { label: 'Executed', value: new Date().toLocaleString() }
+    ]
+  }
+});
