e6d32f800434bf445ec34020d3975eb0a26786cd
11Maks22 <m.slobodianuk@gmail.com>
2025-04-27 03:18:27 +0300

    small fixed for FPj

--- Final_Project/cucumber.mjs
+++ Final_Project/cucumber.mjs
@@ -1,9 +1,11 @@
 export default {
     loader: ['ts-node/esm'],
     format: [
+        'progress',
         '@cucumber/pretty-formatter',
         'json:./reports/cucumber.json',
         'html:./reports/cucumber-embedded.html',
+        'message:allure-results/allure-results.ndjson',
         'junit:./reports/cucumber.xml'
     ],
     formatOptions: {
